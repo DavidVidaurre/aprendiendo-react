3eb21b3c404797bdaf0b5cb6b353913a6388f226
DavidVidaurre <luisdavidmaquenvidaurre@gmail.com>
2023-05-20 20:13:15 -0500

    Add first class

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+export function App() {
+  return (
+    <section className='App'>
+      <TwitterFollowCard
+        userName='midudev'
+        name='Miguel Ángel Durán'
+        initialIsFollowing={false} />
+      <TwitterFollowCard
+        userName='pheralb'
+        name='Pablo Hernadez'
+        initialIsFollowing />
+      <TwitterFollowCard
+        userName='elonmusk'
+        name='Elon Musk'
+        initialIsFollowing />
+    </section>
+  )
+}
---
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+export function TwitterFollowCard({ userName='unknown', name, initialIsFollowing=false }) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+  const text = isFollowing ? 'Siguiendo' : 'Seguir'
+  const buttonClassName = isFollowing
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
+
+
+  const handleButtonClick = () => setIsFollowing(!isFollowing)
+  
+  return (
+    <article className='tw-followCard'>
+      <header className='tw-followCard-header'>
+        <img
+          className='tw-followCard-avatar'
+          src={`https://unavatar.io/${userName}`}
+          alt={`El avatar de ${userName}`} />
+        <div className='tw-followCard-info'>
+          <strong>{name}</strong>
+          <span className='tw-followCard-infoUserName'>@{userName}</span>
+        </div>
+      </header>
+
+      <aside>
+        <button className={buttonClassName} onClick={handleButtonClick}>
+          <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+        </button>
+      </aside>
+    </article>
+  )
+}
